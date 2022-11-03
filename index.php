a58004c13ad3e2160b12edb54b866303822387cc
MarekBavolar <114644407+MarekBavolar@users.noreply.github.com>
2022-11-03 08:23:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
 echo '<h1>TUKE</h1>';
 
-echo '<p>First web on Azure</p>';
+echo '<h1>getParameters page</h1>';
+        
+$sn1 = $_GET["a"];        
+$sn2 = $_GET["b"];        
+        
+$text = "a=" . $sn1 . " b=" . $sn2;        
+$sum = $sn1 + $sn2;
+        
+echo $text;
+echo "<br>";
+echo "Sum:" . $sum;
 
 ?>
