bd993517c70a54c6ec9bb5fe67b733b502f2febe
MarekBavolar <114644407+MarekBavolar@users.noreply.github.com>
2022-11-10 07:49:02 +0100

    lol

--- index.php
+++ index.php
@@ -2,16 +2,4 @@
 
 echo '<h1>TUKE</h1>';
 
-echo '<h1>getParameters page</h1>';
-        
-$sn1 = $_GET[" a "];        
-$sn2 = $_GET[" b "];        
-        
-$text = " a= " . $sn1 . " b= " . $sn2;        
-$sum = $sn1 + $sn2;
-        
-echo $text;
-echo "<br>";
-echo "Sum:" . $sum;
-
 ?>
