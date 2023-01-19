d1e799a1e9407365c65f2827050a5d5c5d2e2f34
MarekBavolar <114644407+MarekBavolar@users.noreply.github.com>
2023-01-19 13:39:46 +0100

    Update final.php

--- final.php
+++ final.php
@@ -1,12 +1,29 @@
 <?php 
-    echo '<h1>getParameters page</h1>';
+    echo '<h1>GET</h1>';
         
     $sn1 = $_GET["a"];        
     $sn2 = $_GET["b"];        
     $sn3 = $_GET["c"];  
+    $sn4 = $_GET["operator"];
             
-    $text = " a= " . $sn1 . " b= " . $sn2 ." c= " . $sn3;        
-    $sum = $sn1 + $sn2 + $sn3;
+    $text = " a= " . $sn1 . " b= " . $sn2 ." c= " . $sn3;  
+    if($sn4 == "+")
+    {
+        $sum = $sn1 + $sn2 + $sn3;
+    }
+    else if($sn4 == "-")
+    {
+        $sum = $sn1 - $sn2 - $sn3;
+    }
+    else if($sn4 == "*")
+    {
+        $sum = $sn1 * $sn2 * $sn3;
+    }
+    else if($sn4 == "/")
+    {
+        $sum = $sn1 / $sn2 / $sn3;
+    }
+    
             
     echo $text;
     echo "<br>";
