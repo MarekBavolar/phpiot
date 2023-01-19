90b71b6aa6d589a7532a909a9725a0a27c3f3c20
MarekBavolar <114644407+MarekBavolar@users.noreply.github.com>
2023-01-19 13:51:40 +0100

    Update final.php

--- final.php
+++ final.php
@@ -1,5 +1,5 @@
 <?php 
-    echo '<h1>-GET-</h1>';
+    echo '<h1>GET</h1>';
         
     $sn1 = $_GET["a"];        
     $sn2 = $_GET["b"];        
@@ -9,11 +9,8 @@
     $text = " a= " . $sn1 . " b= " . $sn2 ." c= " . $sn3;  
     function GET($num1,$num2,$num3,$operator){
         $sum = 0;
-        if($operator == "+")
-        {
-            $sum = $num1 + $num2 + $num3;
-        }
-        else if($operator == "-")
+        
+        if($operator == "-")
         {
             $sum = $num1 - $num2 - $num3;
         }
@@ -24,6 +21,9 @@
         else if($operator == "/")
         {
             $sum = $num1 / $num2 / $num3;
+        }
+        else{
+            $sum = $num1 + $num2 + $num3;
         }
         return $sum;
     }
